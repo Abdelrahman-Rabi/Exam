8e39324a3c8911f6e28dc1c732dd7e7b04d89a07
your_github_username <rabi.abdelrahman@gmail.com>
2021-01-01 21:59:28 +0200

    part 3 solved v1

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -5,32 +5,34 @@ PART 3 [General Information]:
 Type the correct answer below each question:
 
 Q1) Why do we use joins in SQL?
-
+to join columens from different tables 
 
 Q2) Name three types of relationships in databases.
-
+inner join, left join, full join
 
 Q3) What are the main three sections of a JWT?
-
+header, payload, signature 
 
 Q4) Name three types of express middle-wares.
-
+app level middle ware
+function level middle ware
 
 Q5) What is the `effect` hook used for in react?
-
+to perfome code on specifice triger 
 
 Q6) What is the `state` hook used for in react?
-
+to re-render any change on any state directly without need to reload page
 
 Q7) List three thing you can use to create a responsive web application.
-
+grid , flex, using percentage e.g. width 100% insted of fixed value
 
 Q8) Name two of mongoose middle-wares and explain what they are used for
 
 
 Q9) What is the difference between authentication and authorization?
-
+authentication - > to make sure user have permission to access specifice route
+authorization - > to make sure user have permission to do specifice action 
 
 Q10) Why do we save some information as an environment variable (in .env file)?
-
+to make sure variable value can be taken from the system run the application 
 */
